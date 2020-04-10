f712b375f8f5166f6b361450adc5d4aa9b8a543e
Justine Fabre <justinefabre84@gmail.com>
2020-04-10 15:57:48 +0100

    add score sharing with whatsapp

--- src/EndGame/EndGame.js
+++ src/EndGame/EndGame.js
@@ -5,6 +5,7 @@ import {Route, Switch} from "react-router";
 import {Link} from "react-router-dom";
 
 function EndGame(props) {
+
     return (
         <section className="endgame">
             <header>
@@ -22,7 +23,7 @@ function EndGame(props) {
                 </div>
             </div>
 
-            <a href="whatsapp://send?text=Can%20you%20beat%20my%20score%3F%20Test%20your%20coding%20knowledge%20with%20this%20fun%20quiz!" data-event-category="Social" data-event-action="Share:Whatsapp">Share on Whatsapp</a>
+            <a href={`whatsapp://send?text=I%20did%20${props.finalScore}%20on%20this%20fun%20coding%20quiz!%20Can%20you%20beat%20my%20score%3F%20`} data-event-category="Social" data-event-action="Share:Whatsapp">Share on Whatsapp</a>
                 {/*<nav>*/}
                 {/*    <Link to="/">Bring me back home</Link>*/}
                 {/*    <Route exact={true} path="/" component={Home}/>*/}
