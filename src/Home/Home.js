902a4ce8c6846ad8b2f1025129105ddc8f2b4e37
Justine Fabre <justinefabre84@gmail.com>
2020-04-11 20:29:16 +0100

    improved button effect

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -21,8 +21,8 @@ class Home extends Component {
                         <h3 className="home-rules">The rules are simple:</h3>
                         <li className="home-rules-list"> Select your answer</li>
                         <li className="home-rules-list"> If it is the right answer, you get 1 point</li>
-                        <li className="home-rules-list"> If the answer is wrong, you do not lose point</li>
-                        <li className="home-rules-list"> There is a total of 20 points</li>
+                        <li className="home-rules-list"> If the answer is wrong, you do not lose any points</li>
+                        <li className="home-rules-list"> There is a total of 10 points</li>
                     </div>
                     <div className="home-container-column2">
                         <i className="arrow fas fa-arrow-circle-down"></i>
@@ -30,6 +30,7 @@ class Home extends Component {
                             <Link to="/quiz">Start the quiz</Link>
                             <Route path="/quiz" exact component={Quiz}/>
                         </button>
+                        <i className="arrow fas fa-arrow-circle-up"></i>
                     </div>
                 </div>
             </section>
