2d54b740948a817cf6f53918ebcdaf48f7e2a46f
Justine Fabre <justinefabre84@gmail.com>
2020-04-12 16:09:49 +0100

    changed UI homepage

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -15,24 +15,33 @@ class Home extends Component {
     render() {
         return (
             <section className="home">
-                <h1 className="home-welcome">Welcome to this coding quiz</h1>
-                <div className="home-container">
-                    <div className="home-container-column1">
-                        <h3 className="home-rules">The rules are simple:</h3>
-                        <li className="home-rules-list"> Select your answer</li>
-                        <li className="home-rules-list"> If it is the right answer, you get 1 point</li>
-                        <li className="home-rules-list"> If the answer is wrong, you do not lose any points</li>
-                        <li className="home-rules-list"> There is a total of 10 points</li>
+                <div className="home-welcome">
+                    <h1 className="home-welcome-title">Welcome to this</h1>
+                    <h1 className="home-welcome-subtitle">coding quiz</h1>
+                    <img className="home-welcome-image" src="pictures/hello.jpg" alt="hello world"/>
+                </div>
+
+                <div className="home-rules">
+                    <div>
+                        <h3 className="home-rules-title">The rules are simple:</h3>
+                        <div className="home-rules-list">
+                            <li className="home-rules-list-item"> Select your answer</li>
+                            <li className="home-rules-list-item"> Right answer: you get 1 point</li>
+                            <li className="home-rules-list-item"> Wrong answer: you do not lose any points</li>
+                            <li className="home-rules-list-item"> There is a total of 10 points</li>
+                        </div>
                     </div>
-                    <div className="home-container-column2">
-                        <i className="arrow fas fa-arrow-circle-down" aria-hidden="true"></i>
-                        <button className="home-button">
+                </div>
+                <div className="home-start">
+                    <i className="arrow fas fa-arrow-circle-down" aria-hidden="true"></i>
+                    <div>
+                        <button className="home-start-button">
                             <Link to="/quiz">Start the quiz</Link>
                             <Route path="/quiz" exact component={Quiz}/>
                         </button>
-                        <i className="arrow fas fa-arrow-circle-up"></i>
                     </div>
                 </div>
+
             </section>
         );
     }
